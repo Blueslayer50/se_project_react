f590df45691dfcfdb99d0318639513444fe07a0b
Manvir Sidhu <Pokemonstar5001@gmail.com>
2026-05-08 17:52:27 -0400

    Sprint 11 2nd Submit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -11,7 +12,9 @@ function Header({ handleAddClick, weatherData }) {
 
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="App logo" />
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="App logo" />
+      </Link>
 
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
@@ -29,6 +32,10 @@ function Header({ handleAddClick, weatherData }) {
         </button>
 
         <div className="header__user-container">
+          <Link to="/profile" className="header__nav-link">
+            Profile
+          </Link>
+
           <p className="header__username">Lebron James</p>
           <img className="header__avatar" src={avatar} alt="User avatar" />
         </div>
